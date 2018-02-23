654d44db1c901fdb38ce1998ea55caadb5856de8
Petro <petro.prokopovych@vantino.ch>
2018-02-23 15:00:31 +0200

    homework for lesson 4

--- src/lesson4/index.js
+++ src/lesson4/index.js
@@ -3,3 +3,14 @@ import task from './task';
 export default {
   task,
 };
+
+const init = [['a', 1], ['b', 2], ['c', 3]];
+const myMap = task.createMap(init);
+const result = [];
+myMap.forEach((v, k) => result.push([k, v]));
+
+
+
+
+
+
--- src/lesson4/task.js
+++ src/lesson4/task.js
@@ -9,7 +9,29 @@
   mySet = createSet(['a', 'b', 'c'])
 */
 export function createSet(arr) {
-
+    return {
+        values: arr ? arr: [],
+        get size() {
+            return this.values.length;
+        },
+        add(val) {
+            if (!this.values.includes(val)) {
+                this.values.push(val);
+            }
+        },
+        has(val) {
+            return this.values.contains(val);
+        },
+        delete(val) {
+            this.values = this.values.filter(x => x !== val);
+        },
+        forEach(func) {
+            return this.values.map(func);
+        },
+        clear() {
+            this.values = [];
+        }
+    }
 }
 
 /*
@@ -23,10 +45,48 @@ export function createSet(arr) {
   myMap = createMap([['a', 1], ['b', 2], ['c', 3]])
 */
 export function createMap(arr) {
-
+    return {
+        values: arr ? arr: [],
+        get size() {
+            return this.values.length;
+        },
+        set(key, val) {
+            const index = this.values.findIndex(x => x[0] === key);
+            if (~index) {
+                this.values[index][1] = val;
+            } else {
+                this.values.push([key, val]);
+            }
+        },
+        get(key) {
+            const entry =  this.values.find(x => x[0] === key);
+            if (entry) return entry[1];
+        },
+        has(key) {
+            return !!this.values.find(x => x[0] === key);
+        },
+        delete(key) {
+            const index = this.values.findIndex(x => x[0] === key);
+            if (~index) {
+                this.values.splice(index, 1);
+                return true;
+            }
+            return false;
+        },
+        forEach(func) {
+            for (let i of this.values) {
+                console.log(i);
+                func(...i.reverse());
+                i.reverse();
+            }
+        },
+        clear() {
+            this.values = [];
+        }
+    }
 }
 
 export default {
-  createSet,
-  createMap
+    createSet,
+    createMap
 };
